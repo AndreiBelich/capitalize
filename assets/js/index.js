c3a9e313ec7fda29397ec68c188ef2670239a127
AndreiBelich <balichandrey89@gmail.com>
2021-04-07 13:20:18 +0300

    Init project

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Возвращает новую строку где первый символ каждого слова начинается с заглавной буквы
+ * если это возможно.
+ * @param {string} str 
+ * @returns string
+ */
+function capitalize(str){
+  return str.split(" ")
+            .map((item) => item[0].toUpperCase() + item.slice(1))
+            .join(" ");
+}
+
+const sentences = [
+  "some string I need",
+  "to be or not to be",
+  "it's string not a number",
+  "hello dear friend",
+  "it's strange message",
+  "1st place in the world"
+];
+
+sentences.forEach((item) => console.log(capitalize(item)));
